2da0ac31551408b34f2271fa46ed1c65634a3ac0
TonyVk <toni.sikora@live.com>
2022-05-23 22:45:36 +0200

    Dovrseni oglasi

--- resources/oglasi/client/main.lua
+++ resources/oglasi/client/main.lua
@@ -5,31 +5,99 @@ Citizen.CreateThread(function()
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(0)
 	end
+    DodajPeda()
 end)
 
-RegisterCommand("oglas", function(source, args, raw)
-	if args[1] then
-        ESX.TriggerServerCallback('oglasi:DohvatiPodatke', function(ime, prez, broj)
-            SendNUIMessage({
-                dodajoglas = true,
-                ime = ime,
-                prezime = prez,
-                broj = broj,
-                tekst = table.concat(args, " ")
-            })
-        end)
-    else
-        ESX.ShowNotification("Upisite tekst oglasa!")
-    end
+local OglasPed = nil
+
+function DodajPeda()
+    local pedmodel = GetHashKey("csb_reporter")
+    ESX.Streaming.RequestModel(pedmodel)
+    OglasPed = CreatePed(0, pedmodel, -1083.2482910156, -245.91780090332, 36.763233184814, 205.36, false, true)
+    SetEntityInvincible(OglasPed, true)
+    SetBlockingOfNonTemporaryEvents(OglasPed, true)
+    SetPedDiesWhenInjured(OglasPed, false)
+    SetPedFleeAttributes(OglasPed, 2)
+    FreezeEntityPosition(OglasPed, true)
+    SetPedCanPlayAmbientAnims(OglasPed, false)
+    SetPedCanRagdollFromPlayerImpact(OglasPed, false)
+    SetEntityAsNoLongerNeeded(pedmodel)
+
+    local blip = AddBlipForCoord(-1083.2482910156, -245.91780090332, 36.763233184814)
+    SetBlipSprite (blip, 184)
+    SetBlipDisplay(blip, 6)
+    SetBlipColour (blip, 3)
+    SetBlipScale  (blip, 1.4)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString("Oglasnik")
+    EndTextCommandSetBlipName(blip)
+
+    exports.qtarget:AddEntityZone("oglas", OglasPed, 
+    {
+        name="daj_oglas",
+        debugPoly=false,
+        useZ = true
+    }, {
+        options = {
+            {
+                event = "oglas:DajOglas",
+                icon = "far fa-comment",
+                label = "Kupite oglas ($30)"
+            }
+        },
+        distance = 3.5
+    })
+end
+
+RegisterNetEvent('oglas:DajOglas')
+AddEventHandler('oglas:DajOglas', function()
+    ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'daj_oglas', {
+        title = "Upišite tekst oglasa"
+    }, function(data, menu)
+        local tekst = data.value
+        if tekst and #tekst > 0 and not isBlank(tekst) then
+            TriggerServerEvent("oglasi:DodajOglas", tekst)
+        else
+            ESX.ShowNotification("Upišite tekst oglasa!")
+        end
+        menu.close()
+    end, function(data, menu)
+        menu.close()
+    end)
 end)
+
+function isBlank(x)
+    return not not tostring(x):find("^%s*$")
+end
 
 RegisterNetEvent('oglasi:OtvoriNovine')
 AddEventHandler('oglasi:OtvoriNovine', function()
     SetNuiFocus(true, true)
     TriggerEvent("MakniHud", true)
-    SendNUIMessage({
-        prikazi = true
-    })
+    ESX.TriggerServerCallback('oglasi:DajOglase', function(ogl)
+        for i=1, 12, 1 do
+            if ogl[i] ~= nil then
+                SendNUIMessage({
+                    dodajoglas = true,
+                    ime = ogl[i].firstname,
+                    prezime = ogl[i].lastname,
+                    broj = ogl[i].broj,
+                    tekst = ogl[i].tekst
+                })
+            else
+                SendNUIMessage({
+                    dodajoglas = true,
+                    ime = "Ime",
+                    prezime = "Prezime",
+                    broj = "1234567",
+                    tekst = "Ovdje se može nalaziti vaš oglas! Posjetite nas."
+                })
+            end
+        end
+        SendNUIMessage({
+            prikazi = true
+        })
+    end)
 end)
 
 RegisterNUICallback(
--- resources/oglasi/server/main.lua
+++ resources/oglasi/server/main.lua
@@ -2,10 +2,41 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-ESX.RegisterServerCallback('oglasi:DohvatiPodatke', function(source, cb)
+local Oglasi = {}
+
+RegisterNetEvent('oglasi:DodajOglas')
+AddEventHandler('oglasi:DodajOglas', function(tekst)
     local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
-    cb(xPlayer.getFirstname(), xPlayer.getLastname(), xPlayer.getNumber())
+    if xPlayer.getMoney() >= 50 then
+        xPlayer.removeMoney(50)
+        MySQL.Async.insert('INSERT INTO yellow_tweets (phone_number, firstname, lastname, message, novine) VALUES (@br, @ime, @prez, @tekst, 0)',{
+            ['@br'] = xPlayer.getNumber(),
+            ['@ime']  = xPlayer.getFirstname(),
+            ['@prez']  = xPlayer.getLastname(),
+            ['@tekst']  = tekst
+        }, function(insertId)
+            MySQL.Async.fetchAll("select @s:=@s+1 as br, id from yellow_tweets, (SELECT @s:= 0) AS s where novine = 0", {}, function(result)
+                local broj = 0
+                for i=1, #result, 1 do
+                    if result[i] and result[i].id == insertId then
+                        broj = math.floor(tonumber(result[i].br)/12)
+                        break
+                    end
+                end
+                if broj == 0 then
+                    broj = 1
+                end
+                xPlayer.showNotification("Vaš oglas će završiti na oglasniku za "..broj.."h")
+            end)
+        end)
+    else
+        xPlayer.showNotification("Nemate $50.")
+    end
+end)
+
+ESX.RegisterServerCallback('oglasi:DajOglase', function(source, cb)
+    cb(Oglasi)
 end)
 
 ESX.RegisterUsableItem('novine', function(source)
@@ -17,4 +48,22 @@ ESX.RegisterUsableItem('novine', function(source)
         TriggerClientEvent("oglasi:OtvoriNovine", _source)
         xPlayer.removeInventoryItem("novine", 1)
     end
+end)
+
+function DohvatiOglase()
+    Oglasi = {}
+    MySQL.Async.fetchAll("select id, phone_number as broj, firstname, lastname, message as tekst from yellow_tweets where novine = 0 limit 12", {}, function(result)
+		Oglasi = result
+        TriggerClientEvent('chat:addMessage', -1, { args = { 'Novine ', "Izašo je novi broj oglasnika!" } })
+        for i=1, #result, 1 do
+            MySQL.Async.execute('UPDATE yellow_tweets SET novine = 1 WHERE id = @id', {
+				['@id'] = result[i].id
+			})
+        end
+	end)
+    SetTimeout(300000, DohvatiOglase)
+end
+
+MySQL.ready(function ()
+    DohvatiOglase()
 end)
