a63a760855ce2f6580bdc0b79577253f5028a61d
TonyVk <toni.sikora@live.com>
2022-06-22 17:36:42 +0200

    Mobitel animacija

--- resources/mobitel/client/main.lua
+++ resources/mobitel/client/main.lua
@@ -1,5 +1,52 @@
 local Otvoren = false;
 local UPozivu = false;
+local myPedId = nil
+
+local phoneProp = 0
+local phoneModel = "prop_amb_phone"
+-- OR "prop_npc_phone"
+-- OR "prop_npc_phone_02"
+-- OR "prop_cs_phone_01"
+
+local currentStatus = 'out'
+local lastDict = nil
+local lastAnim = nil
+local lastIsFreeze = false
+
+local ANIMS = {
+	['cellphone@'] = {
+		['out'] = {
+			['text'] = 'cellphone_text_in',
+			['call'] = 'cellphone_call_listen_base',
+		},
+		['text'] = {
+			['out'] = 'cellphone_text_out',
+			['text'] = 'cellphone_text_in',
+			['call'] = 'cellphone_text_to_call',
+		},
+		['call'] = {
+			['out'] = 'cellphone_call_out',
+			['text'] = 'cellphone_call_to_text',
+			['call'] = 'cellphone_text_to_call',
+		}
+	},
+	['anim@cellphone@in_car@ps'] = {
+		['out'] = {
+			['text'] = 'cellphone_text_in',
+			['call'] = 'cellphone_call_in',
+		},
+		['text'] = {
+			['out'] = 'cellphone_text_out',
+			['text'] = 'cellphone_text_in',
+			['call'] = 'cellphone_text_to_call',
+		},
+		['call'] = {
+			['out'] = 'cellphone_horizontal_exit',
+			['text'] = 'cellphone_call_to_text',
+			['call'] = 'cellphone_text_to_call',
+		}
+	}
+}
 
 ESX = nil
 
@@ -37,6 +84,95 @@ RegisterNUICallback(
       TriggerServerEvent("mobitel:ObrisiKontakt", data.broj)
     end
 )
+
+function newPhoneProp()
+	deletePhone()
+	RequestModel(phoneModel)
+	while not HasModelLoaded(phoneModel) do
+		Citizen.Wait(1)
+	end
+	phoneProp = CreateObject(GetHashKey(phoneModel), 1.0, 1.0, 1.0, 1, 1, 0)
+	SetModelAsNoLongerNeeded(phoneModel)
+	local bone = GetPedBoneIndex(myPedId, 28422)
+	AttachEntityToEntity(phoneProp, myPedId, bone, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 1, 0, 0, 2, 1)
+end
+
+function deletePhone ()
+	if phoneProp ~= 0 then
+		Citizen.InvokeNative(0xAE3CBE5BF394C9C9 , Citizen.PointerValueIntInitialized(phoneProp))
+		phoneProp = 0
+	end
+end
+
+--[[
+	out || text || Call ||
+--]]
+function PhonePlayAnim (status, freeze, force)
+	if currentStatus == status and force ~= true then
+		return
+	end
+
+	myPedId = GetPlayerPed(-1)
+	local freeze = freeze or false
+
+	local dict = "cellphone@"
+	if IsPedInAnyVehicle(myPedId, false) then
+		dict = "anim@cellphone@in_car@ps"
+	end
+	loadAnimDict(dict)
+
+	local anim = ANIMS[dict][currentStatus][status]
+	if currentStatus ~= 'out' then
+		StopAnimTask(myPedId, lastDict, lastAnim, 1.0)
+	end
+	local flag = 50
+	if freeze == true then
+		flag = 14
+	end
+	TaskPlayAnim(myPedId, dict, anim, 3.0, -1, -1, flag, 0, false, false, false)
+
+	if status ~= 'out' and currentStatus == 'out' then
+		Citizen.Wait(380)
+		newPhoneProp()
+	end
+
+	lastDict = dict
+	lastAnim = anim
+	lastIsFreeze = freeze
+	currentStatus = status
+
+	if status == 'out' then
+		Citizen.Wait(180)
+		deletePhone()
+		StopAnimTask(myPedId, lastDict, lastAnim, 1.0)
+	end
+
+end
+
+function PhonePlayOut ()
+	PhonePlayAnim('out')
+end
+
+function PhonePlayText ()
+	PhonePlayAnim('text')
+end
+
+function PhonePlayCall (freeze)
+	PhonePlayAnim('call', freeze)
+end
+
+function PhonePlayIn () 
+	if currentStatus == 'out' then
+		PhonePlayText()
+	end
+end
+
+function loadAnimDict(dict)
+	RequestAnimDict(dict)
+	while not HasAnimDictLoaded(dict) do
+		Citizen.Wait(1)
+	end
+end
 
 function ImalSignala()
   local centarGrada = vector3(86.756629943848,-989.32501220703,29.407320022583)
@@ -141,6 +277,7 @@ RegisterNUICallback(
     "zatvorioMob",
     function(data, cb)
 		  Otvoren = not Otvoren
+      PhonePlayOut()
       SetNuiFocus(false)
     end
 )
@@ -254,8 +391,10 @@ AddEventHandler('esx:onPlayerDeath', function()
       br = Otvoren
     })
     if Otvoren then
+      PhonePlayIn()
       SetNuiFocus(true, true)
     else
+      PhonePlayOut()
       SetNuiFocus(false)
     end
 	end
@@ -272,6 +411,7 @@ AddEventHandler('mobitel:Nazovi', function(br)
       mobitel = true,
       br = Otvoren
     })
+    PhonePlayIn()
     SetNuiFocus(true, true)
     if ImalSignala() then
       TriggerServerEvent("mobitel:Nazovi", br)
@@ -292,6 +432,7 @@ AddEventHandler('mobitel:PosaljiPoruku', function(br)
       mobitel = true,
       br = Otvoren
     })
+    PhonePlayIn()
     SetNuiFocus(true, true)
     SendNUIMessage({
       nporuka = true,
@@ -313,8 +454,10 @@ RegisterCommand('+mobitel', function()
           })
         end)
         if Otvoren then
+          PhonePlayIn()
           SetNuiFocus(true, true)
         else
+          PhonePlayOut()
           SetNuiFocus(false)
         end
       end
