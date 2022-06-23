b9a3210d349ca23d2553f48b29dc6f3d5868912e
TonyVk <toni.sikora@live.com>
2022-06-23 23:46:55 +0200

    Sansa kod pecanja

--- resources/pecanje/config.lua
+++ resources/pecanje/config.lua
@@ -12,11 +12,11 @@ Config.FishingItems = {
 }
 
 Config.FishTable = {
-	[1] = {["name"] = "fish",           ["prop"] = "a_c_fish",                      ["ped"] = true,     ["chance"] = 25,     ["trash"] = false},
-	[2] = {["name"] = "oilcan",         ["prop"] = "prop_oilcan_01a",               ["ped"] = false,    ["chance"] = 15,     ["trash"] = true},
+	[1] = {["name"] = "fish",           ["prop"] = "a_c_fish",                      ["ped"] = true,     ["chance"] = 35,     ["trash"] = false},
+	[2] = {["name"] = "oilcan",         ["prop"] = "prop_oilcan_01a",               ["ped"] = false,    ["chance"] = 41,     ["trash"] = true},
     [3] = {["name"] = "tank",           ["prop"] = "p_d_scuba_tank_s",              ["ped"] = false,    ["chance"] = 48,     ["trash"] = true},
     [4] = {["name"] = "shoe",           ["prop"] = "prop_ld_shoe_01",               ["ped"] = false,    ["chance"] = 58,     ["trash"] = true},
-    [5] = {["name"] = "tyre",           ["prop"] = "prop_wheel_tyre",               ["ped"] = false,    ["chance"] = 43,     ["trash"] = true},
+    [5] = {["name"] = "tyre",           ["prop"] = "prop_wheel_tyre",               ["ped"] = false,    ["chance"] = 63,     ["trash"] = true},
     [6] = {["name"] = "bottle",         ["prop"] = "v_ind_cs_bottle",               ["ped"] = false,    ["chance"] = 76,     ["trash"] = true},
 	[7] = {["name"] = "dildo",          ["prop"] = "v_res_d_dildo_f",               ["ped"] = false,    ["chance"] = 100,     ["trash"] = true}
 }
