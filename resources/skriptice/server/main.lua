f9444ea1053d8b3efdca82363d1fb9d8f798baa8
TonyVk <toni.sikora@live.com>
2022-11-26 13:45:54 +0100

    Dodano /nosi

--- resources/skriptice/server/main.lua
+++ resources/skriptice/server/main.lua
@@ -54,6 +54,18 @@ AddEventHandler('esx_markeras:endCommunityServiceCommand', function(source)
 	end
 end)
 
+---------CARRY PEOPLE
+RegisterServerEvent('CarryPeople:sync')
+AddEventHandler('CarryPeople:sync', function(target, animationLib,animationLib2, animation, animation2, distans, distans2, height,targetSrc,length,spin,controlFlagSrc,controlFlagTarget,animFlagTarget)
+	TriggerClientEvent('CarryPeople:syncTarget', targetSrc, source, animationLib2, animation2, distans, distans2, height, length,spin,controlFlagTarget,animFlagTarget)
+	TriggerClientEvent('CarryPeople:syncMe', source, animationLib, animation,length,controlFlagSrc,animFlagTarget)
+end)
+
+RegisterServerEvent('CarryPeople:stop')
+AddEventHandler('CarryPeople:stop', function(targetSrc)
+	TriggerClientEvent('CarryPeople:cl_stop', targetSrc)
+end)
+
 -- unjail after time served
 RegisterServerEvent('esx_markeras:finishCommunityService')
 AddEventHandler('esx_markeras:finishCommunityService', function()
