06bfcbf13d2f44b8ef51b16fdf15aa7f4c55ba46
TonyVk <toni.sikora@live.com>
2022-11-27 18:24:56 +0100

    Vrati lovu kranisti

--- resources/[esx]/[poslovi]/[obicni]/esx_kran/server/main.lua
+++ resources/[esx]/[poslovi]/[obicni]/esx_kran/server/main.lua
@@ -6,8 +6,8 @@ AddEventHandler('kran:EoPlace', function()
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	if xPlayer.posao.name == Config.Posao then
-		xPlayer.addMoney(15)
-		local por = "["..os.date("%X").."] ("..GetCurrentResourceName()..") Igrac "..GetPlayerName(_source).."("..xPlayer.identifier..") je dobio $15"
+		xPlayer.addMoney(150)
+		local por = "["..os.date("%X").."] ("..GetCurrentResourceName()..") Igrac "..GetPlayerName(_source).."("..xPlayer.identifier..") je dobio $150"
 		TriggerEvent("SpremiLog", por)
 		TriggerEvent("biznis:StaviUSef", Config.Posao, math.ceil(150*0.30))
     end
